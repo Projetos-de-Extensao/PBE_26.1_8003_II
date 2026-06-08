4ea3d981bdb2dd7398c02cc3a54c411f707e9572
Bento Couto <bentocouto@gmail.com>
2026-06-08 13:46:27 -0300

    cofiguração da autentificação

--- src/backend/AAC_Refeito/aac/settings.py
+++ src/backend/AAC_Refeito/aac/settings.py
@@ -33,11 +33,11 @@ INSTALLED_APPS = [
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
+           'rest_framework.authentication.TokenAuthentication',
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ],    
+           'rest_framework.permissions.IsAuthenticated',
+    ],
    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
