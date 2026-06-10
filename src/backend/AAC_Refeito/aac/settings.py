fd7ddba4eefd361b7e166e4253513411f23e0383
Miguelsz777 <Miguelbarretocampos1@gmail.com>
2026-06-10 15:18:03 -0300

    corsheaders

--- src/backend/AAC_Refeito/aac/settings.py
+++ src/backend/AAC_Refeito/aac/settings.py
@@ -27,6 +27,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'corsheaders',
     'drf_spectacular',
     'core',
 ]
@@ -48,6 +49,7 @@ SPECTACULAR_SETTINGS = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -55,6 +57,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+CORS_ALLOWED_ORIGINS = [
+       "http://localhost:3000", 
 ]
 
 ROOT_URLCONF = 'aac.urls'
